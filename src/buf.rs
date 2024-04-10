4fba0b3581e1c50fe59ccef2f9eecd21bccdb191
CordlessCoder <>
2024-04-10 09:33:46 +0100

    init

---
+++ src/buf.rs
@@ -0,0 +1,152 @@
+use bytemuck::Zeroable;
+
+#[derive(bytemuck::Pod, Zeroable, Clone, Copy)]
+// Required for compatibility with the u32 type
+#[repr(align(4))]
+#[repr(C)]
+#[cfg(target_endian = "little")]
+pub struct Pixel {
+    pub b: u8,
+    pub g: u8,
+    pub r: u8,
+    pub a: u8,
+}
+#[derive(bytemuck::Pod, Zeroable, Clone, Copy)]
+// Required for compatibility with the u32 type
+#[repr(align(4))]
+#[repr(C)]
+#[cfg(target_endian = "big")]
+pub struct Pixel {
+    pub a: u8,
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+impl Pixel {
+    pub const fn black() -> Self {
+        Pixel::new(0, 0, 0, 255)
+    }
+    pub const fn white() -> Self {
+        Pixel::new(255, 255, 255, 255)
+    }
+    pub const fn new(r: u8, g: u8, b: u8, a: u8) -> Self {
+        Pixel { r, g, b, a }
+    }
+}
+
+pub struct Buffer {
+    pixels: Vec<Pixel>,
+    width: usize,
+    height: usize,
+}
+
+impl Buffer {
+    pub fn new(width: usize, height: usize, fill: Pixel) -> Self {
+        Self::new_with(width, height, |_, _| fill)
+    }
+    pub fn new_with<F: FnMut(usize, usize) -> Pixel>(
+        width: usize,
+        height: usize,
+        mut cb: F,
+    ) -> Self {
+        Self {
+            width,
+            height,
+            pixels: (0..height)
+                .flat_map(|y| (0..width).map(move |x| (x, y)))
+                .map(|(x, y)| cb(x, y))
+                .collect(),
+        }
+    }
+    pub fn as_rgba(&self) -> &[u32] {
+        bytemuck::cast_slice(&self.pixels)
+    }
+    pub fn as_rgba_mut(&mut self) -> &mut [u32] {
+        bytemuck::cast_slice_mut(&mut self.pixels)
+    }
+    pub fn inner_buf(&self) -> &[Pixel] {
+        &self.pixels
+    }
+    pub fn inner_buf_mut(&mut self) -> &mut [Pixel] {
+        &mut self.pixels
+    }
+    pub fn width(&self) -> usize {
+        self.width
+    }
+    pub fn height(&self) -> usize {
+        self.height
+    }
+    pub fn get(&self, x: usize, y: usize) -> Option<Pixel> {
+        if x >= self.width || y >= self.height {
+            return None;
+        }
+        Some(unsafe { *self.get_unchecked(x, y) })
+    }
+    pub fn get_mut(&mut self, x: usize, y: usize) -> Option<&mut Pixel> {
+        if x >= self.width || y >= self.height {
+            return None;
+        }
+        Some(unsafe { self.get_unchecked_mut(x, y) })
+    }
+    pub unsafe fn get_unchecked(&self, x: usize, y: usize) -> &Pixel {
+        self.pixels.get_unchecked(y * self.width + x)
+    }
+    pub unsafe fn get_unchecked_mut(&mut self, x: usize, y: usize) -> &mut Pixel {
+        self.pixels.get_unchecked_mut(y * self.width + x)
+    }
+    pub fn set(&mut self, x: usize, y: usize, value: Pixel) {
+        let Some(pixel) = self.get_mut(x, y) else {
+            panic!(
+                "Attempted to set pixel {x}, {y} in a {width}x{height} buffer",
+                width = self.width(),
+                height = self.height()
+            )
+        };
+        *pixel = value;
+    }
+    pub fn iter<'s>(&'s self) -> impl Iterator<Item = &'s Pixel> + 's {
+        self.pixels.iter()
+    }
+    pub fn iter_mut<'s>(&'s mut self) -> impl Iterator<Item = &'s mut Pixel> + 's {
+        self.pixels.iter_mut()
+    }
+    pub fn iter_pos<'s>(&'s self) -> impl Iterator<Item = (usize, usize, &'s Pixel)> + 's {
+        self.pixels
+            .chunks_exact(self.width)
+            .enumerate()
+            .flat_map(|(y, row)| row.iter().enumerate().map(move |(x, pixel)| (x, y, pixel)))
+    }
+    pub fn iter_pos_mut<'s>(
+        &'s mut self,
+    ) -> impl Iterator<Item = (usize, usize, &'s mut Pixel)> + 's {
+        self.pixels
+            .chunks_exact_mut(self.width)
+            .enumerate()
+            .flat_map(|(y, row)| {
+                row.into_iter()
+                    .enumerate()
+                    .map(move |(x, pixel)| (x, y, pixel))
+            })
+    }
+    pub fn resize_and_fill<F: FnMut(usize, usize) -> Pixel>(
+        &mut self,
+        width: usize,
+        height: usize,
+        mut cb: F,
+    ) {
+        if self.width == width && self.height == height {
+            return;
+        }
+        self.pixels.clear();
+        let iter = (0..height)
+            .flat_map(|y| (0..width).map(move |x| (x, y)))
+            .map(|(x, y)| cb(x, y));
+        self.pixels.extend(iter);
+        self.width = width;
+        self.height = height;
+    }
+    pub fn resize(&mut self, width: usize, height: usize) {
+        self.resize_and_fill(width, height, |_, _| Pixel::black())
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+mod buf;
+
+use buf::{Buffer, Pixel};
+use minifb::{Key, Window, WindowOptions};
+use std::time::Duration;
+
+fn main() {
+    const WIDTH: usize = 32;
+    const HEIGHT: usize = 32;
+    const WIN_WIDTH: usize = 256;
+    const WIN_HEIGHT: usize = 256;
+    const FIXED_SIZE: bool = true;
+
+    let mut window = Window::new(
+        "renderer",
+        WIN_WIDTH,
+        WIN_HEIGHT,
+        WindowOptions {
+            borderless: true,
+            resize: true,
+            scale: minifb::Scale::FitScreen,
+            ..WindowOptions::default()
+        },
+    )
+    .unwrap();
+
+    let mut buf = Buffer::new(WIDTH, HEIGHT, Pixel::black());
+
+    window.limit_update_rate(Some(Duration::from_nanos(1_000_000_000 / 240)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        if !FIXED_SIZE {
+            let (width, height) = window.get_size();
+            buf.resize(width, height);
+        }
+        let (width, height) = (buf.width(), buf.height());
+        buf.iter_pos_mut().for_each(|(x, y, p)| {
+            let uv = (x as f32 / width as f32, y as f32 / height as f32);
+            p.r = (uv.0 * (u8::MAX as f32)) as u8;
+            p.g = (uv.1 * (u8::MAX as f32)) as u8;
+            p.b = 0;
+        });
+
+        window
+            .update_with_buffer(buf.as_rgba(), width, height)
+            .unwrap();
+    }
+}
